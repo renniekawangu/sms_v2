ec60aedc595a445369d0aa967c780e575111d274
rennie kawangu <rennie@kali.kali>
2026-01-14 01:51:55 +0200

    a003 commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -42,7 +42,7 @@ function Header({ onMenuClick }) {
           </div>
         </div>
         <div className="flex items-center gap-2 lg:gap-4 ml-auto">
-          <img src="/logo.png" alt="Esync Logo" className="w-40 h-24 object-contain ml-4 border-l border-gray-200 pl-4" />
+          <img src="/logo.png" alt="Esync Logo" className="w-40 h-20 object-contain ml-4 border-l border-gray-200 pl-4" />
         </div>
       </div>
     </header>
